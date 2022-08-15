aaff89c97bf44b6f8258ecdcd3487c3b15cc3782
Jmok19927 <jmok19927@gmail.com>
2022-08-15 16:29:18 -0700

    working basic note routing, adjusting git ignore

--- controllers/index.ts
+++
@@ -1,5 +0,0 @@
-// this is just a test to show the structure
-
-exports.emailExists = require('./emailExists.js');
-exports.createUser = require('./createUser.js');
-exports.matchPassword = require('./matchPassword.js');
