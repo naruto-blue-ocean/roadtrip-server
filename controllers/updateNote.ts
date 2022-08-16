8c54a42f5f94e35a6abf4147c15567fea1f45592
Louisaflor <louisa.yonzon6@gmail.com>
2022-08-16 15:14:29 -0700

    created updated notes file

---
+++ controllers/updateNote.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+// import client from '../db/index.js';
+// const client = require('../db/index.js');
+
+
+const updateNote = (req : any, res : any) => {
+  console.log("it goes in here")
+}
+
+module.exports = updateNote;
