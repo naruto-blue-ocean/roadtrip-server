7cafb8a24f3f6fde9392f8fbe16da1c9f7d7cfa2
noattongi <noattongi@gmail.com>
2022-08-17 16:45:20 -0700

    added seeding files

--- db/seed.sql
+++ db/seed.sql
@@ -5,7 +5,10 @@ INSERT INTO users (email, password) VALUES ('testuser@email.com', 'testuserpass'
 INSERT INTO users (email, password) VALUES ('testemail', 'testpassword');
 INSERT INTO users (email, password) VALUES ('noa@email.com', 'noapass')
 
-INSERT INTO trips (name, status)
+WITH id as (INSERT INTO trips (name, status) VALUES ('THE TRIPPIEST TRIP', 'planned') RETURNING id)
+INSERT INTO user_trip (user_email, trip_id) VALUES ('noa@email.com', id);
+WITH id as (INSERT INTO trips (name, status) VALUES ('The 4th Great Ninja War', 'planned') RETURNING id)
+INSERT INTO user_trip (user_email, trip_id) VALUES ('noa@email.com', id);
 
 INSERT INTO pois (id, name) VALUES ('jIxS5Td2o0gBWx0G0qx59Q', 'San Jose Municipal Rose Garden');
 INSERT INTO pois (id, name) VALUES ('testid', 'testpoiname');
