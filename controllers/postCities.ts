3da93f9fbd7b63934a90a9fecd54aaa66490f081
JASPER <jasjasper101@gmail.com>
2022-08-17 09:34:56 -0700

    chore: update commit

--- controllers/postCities.ts
+++ controllers/postCities.ts
@@ -2,12 +2,12 @@ var client = require('../db/index.js');
 var express = require('express');
 
 const postCities = async (req: any, res: any) => {
-  type CityInfo = {name: string, id: string};
+  type CityInfo = {name: string, id: string, lat: number, lng: number};
 
   req.body.forEach(async (city: CityInfo) => {
-    var queryString = `INSERT INTO destinations(name, id)
-    VALUES ($1, $2)`;
-    var values = [city.name, city.id]
+    var queryString = `INSERT INTO destinations(name, id, lat, lng)
+    VALUES ($1, $2, $3, $4)`;
+    var values = [city.name, city.id, city.lat, city.lng]
     var results = await client.query(queryString, values)
     try{console.log('successfully stored data')}
     catch(err) {console.log('err in storing data', err)}
